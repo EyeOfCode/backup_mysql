8bb35982899f6d67a66c874737b55e124b3c4060
Yodsawat Santhi <yodsawat.s@siamtv.co.th>
2024-10-08 09:35:05 +0700

    first commit

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+chmod -x
+
+# Load environment variables from .env file
+source .env
+
+# Get current date and time
+DATE=$(date +"%Y%m%d%H%M%S")
+
+# Create backup
+mysqldump -u $DB_USER -p$DB_PASS $DB_NAME > $BACKUP_DIR/${DB_NAME}_backup_$DATE.sql
+
+# Optional: Remove backups older than 7 days
+find $BACKUP_DIR -type f -name "*.sql" -mtime +7 -exec rm {} \;
+
+echo "Backup of $DB_NAME completed and saved to $BACKUP_DIR/${DB_NAME}_backup_$DATE.sql"
